6f63704d757979de2f9286f9fb12debd2875091c
Anjali Singh <62153950+pikachu28@users.noreply.github.com>
2021-02-08 17:51:54 +0530

    Update menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -260,8 +260,8 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
-              Resume
+            <a href="http://skillzera.tech/" className="resume-link">
+              SkillzEra
             </a>
           </nav>
         </StyledSidebar>
