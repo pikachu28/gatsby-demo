6cf768ebe463f733aafb6a9513cd87a9dc3a3419
Anjali Singh <62153950+pikachu28@users.noreply.github.com>
2021-02-08 12:30:45 +0530

    Add files via upload

---
+++ src/components/sections/hero.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import styled from 'styled-components';
+import { email } from '@config';
+import { navDelay, loaderDelay } from '@utils';
+
+const StyledHeroSection = styled.section`
+  ${({ theme }) => theme.mixins.flexCenter};
+  flex-direction: column;
+  align-items: flex-start;
+  min-height: 100vh;
+
+  h1 {
+    margin: 0 0 30px 4px;
+    color: var(--green);
+    font-family: var(--font-mono);
+    font-size: clamp(var(--fz-sm), 5vw, var(--fz-md));
+    font-weight: 400;
+
+    @media (max-width: 480px) {
+      margin: 0 0 20px 2px;
+    }
+  }
+
+  h3 {
+    margin-top: 10px;
+    color: var(--slate);
+    line-height: 0.9;
+  }
+
+  p {
+    margin: 20px 0 0;
+    max-width: 500px;
+  }
+
+  .email-link {
+    ${({ theme }) => theme.mixins.bigButton};
+    margin-top: 50px;
+  }
+`;
+
+const Hero = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsMounted(true), navDelay);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const one = <h1>Greeting, We are</h1>;
+  const two = <h2 className="big-heading">Xero Technologies</h2>;
+  const three = <h3 className="big-heading">Leap towards Innovation.</h3>;
+  const four = (
+    <p>
+      Our mission is to make professionals around the globe proficient and future-ready.
+      Our vision is to make signicant advancements in technology so that we can make educational
+      and Career opportunities accessible to almost anyone.
+    </p>
+  );
+  const five = (
+    <a href= " https://wa.me/message/VQX4YB3VKS3RE1" className="email-link" target="_blank">
+      Discover Greatness
+    </a>
+  );
+
+  const items = [one, two, three, four, five];
+
+  return (
+    <StyledHeroSection>
+      <TransitionGroup component={null}>
+        {isMounted &&
+          items.map((item, i) => (
+            <CSSTransition key={i} classNames="fadeup" timeout={loaderDelay}>
+              <div style={{ transitionDelay: `${i + 1}00ms` }}>{item}</div>
+            </CSSTransition>
+          ))}
+      </TransitionGroup>
+    </StyledHeroSection>
+  );
+};
+
+export default Hero;
